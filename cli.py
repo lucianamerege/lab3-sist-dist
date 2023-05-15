7270aa13d4abe7577849c5edec7594bee11136d1
Luciana <52220617+lucianamerege@users.noreply.github.com>
2023-05-14 23:28:21 -0300

    Add files via upload

---
+++ cli.py
@@ -0,0 +1,37 @@
+#servidor de echo: lado cliente
+import rpyc
+
+HOST = 'localhost' # maquina onde esta o servidor
+PORT = 10020       # porta que o servidor esta escutando
+
+def iniciaCliente():
+	'''Conecta-se ao servidor.
+	Saida: retorna a conexao criada.'''
+	conn = rpyc.connect(HOST, PORT) 
+	
+	print(type(conn.root)) # mostra que conn.root eh um stub de cliente
+	print(conn.root.get_service_name()) # exibe o nome da classe (servico) oferecido
+
+	return conn
+
+def fazRequisicoes(conn):
+	# le as mensagens do usuario ate ele digitar 'fim'
+	while True: 
+		msg = input("\nO que deseja fazer?\nOpções:\n > Adicionar palavra/significado: Digite 'Add <palavra> <significado>'\n > Listar significados: Digite 'List <palavra>'\n > Fechar conexão: Digite 'Fim'\n > Deletar palavra: Digite 'Del <palavra>'\n\n")
+		if msg == 'Fim': break 
+
+		# envia a mensagem do usuario para o servidor
+		ret = conn.root.exposed_atendeRequisicoes(msg)
+
+		print(ret)
+
+	# encerra a conexao
+	conn.close()
+
+def main():
+	#inicia o cliente
+	conn = iniciaCliente()
+	#interage com o servidor ate encerrar
+	fazRequisicoes(conn)
+
+main()
---
+++ server.py
@@ -0,0 +1,123 @@
+import rpyc
+import select
+import sys
+import threading
+
+#servidor que dispara um processo filho a cada conexao
+from rpyc.utils.server import ForkingServer
+
+PORT = 10020 # porta de acesso
+
+
+class Dict(rpyc.Service):
+	dicionario = {}
+	num_linhas = 0
+
+	def iniciaDicionario(self, nada):
+		with open('dicionario.txt', 'r') as arq:
+			linhas = arq.readlines()
+			for i, linha in enumerate(linhas):
+				if linha != '\n':
+					#strip tira os espaços no começo e fim da linha
+					chave, *valores = [entrada.strip() for entrada in linha.split(',')]
+					valores.insert(0, i)
+					self.dicionario[chave] = valores
+				self.num_linhas = self.num_linhas + 1
+
+		print("Dicionario preparado:")
+		print(self.dicionario)
+
+	def on_connect(self, conn):
+		self.iniciaDicionario(self)
+		print("Cliente conectado.")
+
+	def on_disconnect(self, conn):
+		print("Cliente desconectou.")
+
+	def exposed_atendeRequisicoes(self, msg):
+		#recebe dados do cliente, volta a ser infinito pois está atendendo apenas a cada thread
+		while True:
+			
+			if msg.startswith('Add'):
+				resposta = self.checaAdicao(msg)
+				return resposta
+			elif msg.startswith('List'):
+				resposta = self.retornaLista(msg.split())
+				return resposta
+			elif msg.startswith("Del"):
+				resposta = self.deleta(msg.split())
+				return resposta				
+			else:
+				return 'Comando inexistente.'
+
+	def deleta(self, msg):
+		if len(msg) != 2:
+			return 'Sintaxe incorreta.'
+		elif msg[1] not in self.dicionario:
+			return 'Sintaxe incorreta.'
+		else:
+			values = self.dicionario[msg[1]]
+			lines = open('dicionario.txt', 'r').readlines()
+			lines[values[0]] = '\n'
+			arq = open('dicionario.txt', 'w')
+			arq.writelines(lines)
+			arq.close()
+			del self.dicionario[msg[1]]
+			resposta = msg[1] + ' removido do dicionário'
+			return resposta
+
+	def retornaLista(self, chave):
+		if len(chave) != 2:
+			return 'Sintaxe incorreta.'
+		else:	
+			if chave[1] in self.dicionario:
+				sorted = self.dicionario[chave[1]]
+				del sorted[0]
+				sorted.sort()
+				self.dicionario.update({chave[1]:sorted})
+				return self.dicionario[chave[1]]
+			else:
+				return '[]'
+			
+	def checaAdicao(self, cmd):
+		add = cmd.split()
+		if len(add) != 3:
+			return "Erro", "Erro"
+		elif add[1] in self.dicionario:
+			definicoes = self.dicionario[add[1]]
+			definicoes.append(add[2])
+			self.dicionario.update({add[1]:definicoes})
+
+			with open('dicionario.txt', 'r+') as arq:
+				linhas = arq.readlines()
+				#o primeiro valor da lista é o número da linha onde está aquela palavra
+				linhas[definicoes[0]] = linhas[definicoes[0]].strip() + ', ' + add[2] + '\n'
+				arq.seek(0)
+				for linha in linhas:
+					arq.write(linha)
+
+			resp = "Definição adicionada: " + add[2]
+			return resp
+		else:
+			with open('dicionario.txt', 'a') as arq:
+				arq.write(add[1] + ', ' + add[2] + '\n')
+
+			definicoes = [self.num_linhas]
+			definicoes.append(add[2])
+			self.dicionario.update({add[1]:definicoes})
+
+			self.num_linhas = self.num_linhas + 1
+
+			print(self.dicionario)
+
+			resp = "Palavra adicionada ao dicionario: " + add[1]
+			return resp
+
+
+def main():
+	print("Pronto para receber conexoes...")
+	srv = ForkingServer(Dict, port = PORT)
+	srv.start()
+
+
+main()
